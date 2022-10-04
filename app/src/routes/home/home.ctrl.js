378db27aa20c05714a6bb4bd65391d72cd0e861f
hindong <gowls980807@naver.com>
2022-10-04 22:35:32 +0900

    업데이트

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,42 @@
 "use strict"
 
-const homectr = (request, response)=>{
-    response.render("home/index");
-}
+const users = {
+    id : ["woorimiT", "나개발", "김팀장"],
+    password : ["1234", "1234", "123456"],
+};
 
-const login = (request, response)=>{
-    response.render("home/login");
-}
+const output = {
+    home : (request, response)=>{
+        response.render("home/index");
+    },
+    login : (request, response)=>{
+        response.render("home/login");
+    }
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id;
+        const password = req.body.password;
+
+        if( users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.password[idx] === password){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
+};
 
 module.exports = {
-    homectr,
-    login,
+    output,
+    process,
 };
 
